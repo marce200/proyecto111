fbd0c079ff6c2118745fe89db7dcbd66633ffc6c
ROOT <ROOT@WIN7ENT2022>
2022-03-17 16:57:09 +0100

    Primera version

---
+++ prueba111/src/prueba111/Principal111.java
@@ -0,0 +1,9 @@
+package prueba111;
+
+public class Principal111 {
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo Actual");
+		System.out.println("Adiso Mundo Actual");
+		 }
+		 
+	}
