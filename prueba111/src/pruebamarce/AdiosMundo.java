29e13dd8aaf6df7d042aaba213a95e24112927a0
marce200 <marceceinmark@hotmailcom>
2022-03-20 02:42:15 +0100

    Solo afecta rama desde usu1

--- prueba111/src/pruebamarce/AdiosMundo.java
+++ prueba111/src/pruebamarce/AdiosMundo.java
@@ -4,5 +4,6 @@ public class AdiosMundo {
 	  public static void main(String[] args) {
 		  System.out.println("Adios Mundo");
 		  System.out.println("Agur Mundo");
+		  System.out.println("Solo afecta a rama);
 }
 }
